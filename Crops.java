04f25586b00f3b6236a255bbeafb0fd6c8f4f7d3
Kyle Lee <klee50@stuy.edu>
2024-05-23 10:01:18 -0400

    Created crops class and random way to pick a crop

--- Crops.java
+++ Crops.java
@@ -2,8 +2,11 @@ public class Crops{
     String cropName;
     int growthStatus;
     public Crops(){
-
-
+        String[] cropNames = {"Corn", "Wheat","Grain"};
+        int[] growthRates = {1,2,3};
+        int rand = (int)(Math.random()*3);
+        this.cropName = cropNames[rand];
+        this.growthStatus = growthRates[rand];
     }
 
 }
